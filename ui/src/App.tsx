a4e3d5f04a9a4d3e813ceef51310095b51131500
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-05-08 21:22:07 +0530

    react router and shadcn added

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button>Add</Button>
+    </h1>
+  );
 };
 
 export default App;
