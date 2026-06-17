4bfa614f5ed990184ba8a5887c7b837fabbd90b5
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-06-17 13:05:43 +0000

    ui and user service added

---
+++ apps/user-service/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.json({ status: "ok" });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`User Service running on port ${PORT}`);
+});
