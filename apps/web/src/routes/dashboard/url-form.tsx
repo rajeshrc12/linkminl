a4a6fe6e086989af27c6357186e7c4f8b783eba6
rajeshrc12 <rajesh.charhajari@gmail.com>
2026-06-19 03:49:19 +0000

    web: url-form updated

--- apps/web/src/routes/dashboard/url-form.tsx
+++ apps/web/src/routes/dashboard/url-form.tsx
@@ -16,15 +16,20 @@ const UrlForm = () => {
       <div className="flex flex-col p-3 w-175 pt-20 gap-5">
         <h3 className="text-3xl">Create new link</h3>
         <div className="flex flex-col bg-white p-3 shadow rounded-2xl gap-5">
-          <Input
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            type="text"
-            placeholder="enter url"
-          />
-          <div className="flex justify-end">
-            <Button onClick={sendUrl}>Create your link</Button>
-          </div>
+          <form action={sendUrl}>
+            <Input
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              type="text"
+              placeholder="enter url"
+              pattern="[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}"
+              title="Enter a valid domain name like example.com or sub.example.org"
+              required
+            />
+            <div className="flex justify-end">
+              <Button type="submit">Create your link</Button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
